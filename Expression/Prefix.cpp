dc93eb2ee2f85f47b90d845aa224eaf35e7424d9
kerong <70834651+kerong2002@users.noreply.github.com>
2023-04-11 08:20:52 +0800

    Add files via upload

---
+++ Expression/Prefix.cpp
@@ -0,0 +1,49 @@
+//2023/04/10 kerong
+/*
+   Data_Structure : Prefix
+    Time complexity: O(n)
+   Space complexity: O(n)
+*/
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+int evalPrefix(vector<string> tokens) {
+    stack<int>  s;
+    for(int x=tokens.size()-1;x>=0;x--){
+        if(isdigit(tokens[x][tokens[x].length()-1])){
+            s.push(stoi(tokens[x]));
+        }
+        else{
+            int top1 = s.top();
+            s.pop();
+            int top2 = s.top();
+            s.pop();
+            int ans = 0;
+            switch(tokens[x][0]){
+                case '+':
+                    ans = top1 + top2;
+                    break;
+                case '-':
+                    ans = top1 - top2;
+                    break;
+                case '*':
+                    ans = top1 * top2;
+                    break;
+                case '/':
+                    ans = top1 / top2;
+                    break;
+            }
+            s.push(ans);
+        }
+    }
+    return s.top();
+}
+
+int main(){
+    //2*3 - 4/5
+    vector<string> tokens = {"-", "*", "2", "3", "/", "4", "5"};
+    cout << evalPrefix(tokens);
+    return 0;
+}
